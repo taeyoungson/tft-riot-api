d49067478c80d2de90ddd462f164fa2fc2256884
taeyoung son <ty.son@postech.ac.kr>
2024-10-31 02:27:26 +0900

    fix: example.py and README.md

--- example.py
+++ example.py
@@ -25,24 +25,30 @@ def main():
     # TODO(ty.son): temporally get api key from .env
     dotenv.load_dotenv("./riot/.env")
 
+    # 1. Initialize client
     riot_api_client = client.RiotApiClient(api_key=os.getenv("RIOT_API_KEY"))
 
+    # 2. Get challenger-tier league data.
     challenger_league_data = riot_api_client.get_league_data_by_tier(
         tier=types.TierType.CHALLENGER, **_DEFAULT_SEARCH_CONFIG
     )
-    # summoner_ids = [e.summoner_id if not e.inactive for e in challenger_league_data.entries]
 
+    # 3. Collect summoner ids of active challngers
     summoner_ids = []
     for entry in challenger_league_data.entries:
         if not entry.inactive:
             summoner_ids.append(entry.summoner_id)
 
-    # TODO(ty.son) fix rate limit exceeding
+    # 4. Get summoner data by summoner ids
     summoner_data = riot_api_client.get_summoner_data_by_summoner_ids(summoner_ids[:5], **_DEFAULT_SEARCH_CONFIG)
+
+    # 5. Collect puuids from summoner data
     puuids = [d.puuid for d in summoner_data]
+
+    # 6. Get Match Ids by puuids.
     match_ids = riot_api_client.get_match_ids_by_puuids(puuids, **_DEFAULT_SEARCH_CONFIG)
 
-    # check for first summoner
+    # 7. Get Match Datas by match ids
     match_data = riot_api_client.get_match_data_by_match_ids(match_ids[0], **_DEFAULT_SEARCH_CONFIG)
 
     logger.info(
